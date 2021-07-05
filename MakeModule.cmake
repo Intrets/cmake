afd49136e3cefdb4d5378a14aa21cd946058b899
Intrets <geen.inspiratie824@gmail.com>
2021-07-06 01:47:48 +0200

    add compile definition for checking features in code

--- MakeModule.cmake
+++ MakeModule.cmake
@@ -30,4 +30,6 @@ function(make_module)
 	target_link_libraries(${_MODULE_NAME} PUBLIC "${_REQUIRED_LIBS}")
 	target_link_libraries(${_MODULE_NAME} PUBLIC "${_OPTIONAL_LIBS}")
 
+	string(TOUPPER ${_MODULE_NAME} LIB_NAME)
+	add_compile_definitions(LIB_${LIB_NAME})
 endfunction()
