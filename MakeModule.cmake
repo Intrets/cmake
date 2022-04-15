f8b03f8a260dbce968fd24b9584e36384b1b11e9
Intrets <geen.inspiratie824@gmail.com>
2022-04-15 02:50:08 +0200

    fix not specifying library type

--- MakeModule.cmake
+++ MakeModule.cmake
@@ -15,19 +15,23 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 function(make_module)
-	set(optionalArgs)
+	set(optionalArgs LIBRARY_TYPE)
 	set(oneValueArgs MODULE_NAME CXX_STANDARD)
 	set(multiValueArgs MODULE_FILES REQUIRED_LIBS OPTIONAL_LIBS)
 	cmake_parse_arguments("" "${optionalArgs}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
 	message(STATUS "begin making module ${MODULE_NAME}")
 
+	if (NOT ${_LIBRARY_TYPE})
+		set(_LIBRARY_TYPE STATIC)
+	endif()
+
 	foreach(FILE ${_MODULE_FILES})
 		list(APPEND SOURCES_LIST "${FILE}.cpp")
 		list(APPEND HEADERS_LIST "include/${MODULE_NAME}/${FILE}.h")
 	endforeach()
 
-	add_library(${_MODULE_NAME} ${SOURCES_LIST} ${HEADERS_LIST})
+	add_library(${_MODULE_NAME} ${_LIBRARY_TYPE} ${SOURCES_LIST} ${HEADERS_LIST})
 	target_compile_features(${_MODULE_NAME} PUBLIC cxx_std_${_CXX_STANDARD})
 
 	target_include_directories(${_MODULE_NAME} PUBLIC include)
